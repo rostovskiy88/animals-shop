420cdde1e749d164e2f6b7bc21412dcd95b3e473
Oleg Rostovskyi <rostovskiyoleg88@gmail.com>
2021-11-09 01:04:40 +0200

    search feature added

--- list/view.js
+++ list/view.js
@@ -1,28 +1,100 @@
-export default class View {}
+export default class View {
+  domElems = [
+    { name: 'cardsContainer', selector: '.cards' },
+    { name: 'searchInput', selector: '#search-input' },
+    { name: 'animalsModal', selector: '#exampleModal' },
+  ];
 
-<div class="card m-3" style="width: 275px; height: 500px">
-  <h5 class="card-header text-center">Британская короткошерстная</h5>
-  <img
-    src="https://media.istockphoto.com/photos/front-view-of-british-shorthair-cat-7-months-old-sitting-picture-id104355461?k=20&m=104355461&s=612x612&w=0&h=9KPbuZ6tTUxqi-_WZT8zrjNrf1W8XBuXIw8ZIM_YQuI="
-    class="card-img-top"
-    alt="Animal"
-  />
-  <div class="card-body text-center">
-    <p class="card-text h4">Стоимость: 1500 UAH</p>
-    <a
-      href="#"
-      class="btn btn-outline-dark"
-      data-bs-toggle="modal"
-      data-bs-target="#exampleModal"
-    >
-      Узнать больше
-    </a>
-    <a href="#" class="btn btn-outline-dark">
-      <i class="fas fa-shopping-cart"></i>
-    </a>
-  </div>
-  <div class="card-footer text-center">
-    <span class="badge bg-danger">Жен</span>
-    <span class="badge bg-success">Коты</span>
-  </div>
-</div>;
+  linkDomElem() {
+    this.dom = this.domElems.reduce((acc, current) => {
+      acc[current.name] = document.querySelector(current.selector);
+      console.log(acc);
+      return acc;
+    }, {});
+  }
+
+  constructor(changeSearch, openModal) {
+    this.linkDomElem();
+    this.dom.searchInput.addEventListener('change', changeSearch);
+    this.openModal = openModal;
+  }
+
+  async renderList(list) {
+    const renderedHTML = list.map((el) => this.renderCard(el));
+    this.dom.cardsContainer.innerHTML = await renderedHTML.join('');
+  }
+
+  getSearchData() {
+    console.log(this.dom.searchInput.value);
+    return this.dom.searchInput.value;
+  }
+
+  getProductId(event) {
+    return event.target.attributes['data-product-id'].value;
+  }
+
+  createModal = async (data) => {
+    const modalHTML = await this.renderModal(data);
+    this.dom.productModal.innerHTML = modalHTML;
+  };
+
+  renderCard(data) {
+    return `<div id="${data.id}" class="card m-3" style="width: 275px; height: 500px">
+        <h5 class="card-header text-center overflow-hidden">${data.breed}</h5>
+        <img src="${data.image}" class="card-img-top" alt="animal" />
+        <div class="card-body text-center">
+          <p class="card-text h4">Стоимость: ${data.cost} грн</p>
+          <a
+            href="#"
+            class="btn btn-outline-dark"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            Узнать больше
+          </a>
+          <a href="#" class="btn btn-outline-dark">
+            <i class="fas fa-shopping-cart"></i>
+          </a>
+        </div>
+        <div class="card-footer text-center">
+          <span class="badge bg-danger">${data.gender}</span>
+          <span class="badge bg-success">${data.category}</span>
+        </div>
+      </div>`;
+  }
+
+  renderModal(data) {
+    return `<div id="${data.id}" class="card m-3" style="width: 275px; height: 500px">
+        <h5 class="card-header text-center overflow-hidden">${data.breed}</h5>
+        <img src="${data.image}" class="card-img-top" alt="animal" />
+        <div class="card-body text-center">
+          <p class="card-text h4">Стоимость: ${data.cost} грн</p>
+          <a
+            href="#"
+            class="btn btn-outline-dark"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            Узнать больше
+          </a>
+          <a href="#" class="btn btn-outline-dark">
+            <i class="fas fa-shopping-cart"></i>
+          </a>
+        </div>
+        <div class="card-footer text-center">
+          <span class="badge bg-danger">${data.gender}</span>
+          <span class="badge bg-success">${data.category}</span>
+        </div>
+      </div>`;
+  }
+
+  // document.addEventListener('DOMContentLoaded', function(){ // Аналог $(document).ready(function(){
+  //   // Если должен быть найден один элемент
+  //   if((e = document.querySelector("#form_error_message_frontend + div > div:last-child label")) !== null)
+  //     e.classList.add('last'); // Аналог выборки и присвоения класса
+  //   // Если элементов будет много
+  //   Array.prototype.forEach.call(document.querySelectorAll("#form_error_message_frontend + div > div:last-child label"), function(e){
+  //    e.classList.add('last');
+  //   });
+  // });
+}
